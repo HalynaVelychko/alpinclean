d343381a0508594774749fbb8440a66d08be5873
velychko <galyna12.velychko@gmail.com>
2020-06-15 16:52:17 +0300

    added modal windows create by JS

--- js/index.js
+++ js/index.js
@@ -20,3 +20,78 @@ document.querySelector(".btn-calc").addEventListener("click", () => {
   );
   resultPrice.innerHTML = `Ваша ціна за обраними параметрами складає ${price} грн`;
 });
+
+
+//Modal windows
+const firstModal = document.querySelector('#myModalFirst');
+const secondModal = document.querySelector('#myModalSecond');
+const thirdModal = document.querySelector('#myModalThird');
+const fourthModal = document.querySelector('#myModalFourth');
+const fifthModal = document.querySelector('#myModalFifth');
+const sixthModal = document.querySelector('#myModalSixth');
+
+const firstBtn = document.querySelector('#first-btn');
+const secondBtn = document.querySelector('#second-btn');
+const thirdBtn = document.querySelector('#third-btn');
+const fourthBtn = document.querySelector('#fourth-btn');
+const fifthBtn = document.querySelector('#fifth-btn');
+const sixthBtn = document.querySelector('#sixth-btn');
+
+const firstClose = document.querySelector('#closeFirst');
+const secondClose = document.querySelector('#closeSecond');
+const thirdClose = document.querySelector('#closeThird');
+const fourthClose = document.querySelector('#closeFourth');
+const fifthClose = document.querySelector('#closeFifth');
+const sixthClose = document.querySelector('#closeSixth');
+
+document
+    .querySelectorAll('.more-information')
+    .forEach(button => button.addEventListener('click', openModaWindow));
+
+function openModaWindow (ev){
+  if(firstBtn ===ev.target) {
+    firstModal.style.display = "block";
+  }
+  if(secondBtn ===ev.target) {
+    secondModal.style.display = "block";
+  }
+  if(thirdBtn ===ev.target) {
+    thirdModal.style.display = "block";
+  }
+  if(fourthBtn ===ev.target) {
+    fourthModal.style.display = "block";
+  }
+  if(fifthBtn ===ev.target) {
+    fifthModal.style.display = "block";
+  }
+  if(sixthBtn===ev.target) {
+    sixthModal.style.display = "block";
+  }
+}
+
+document
+    .querySelectorAll('.closeBtn')
+    .forEach(span => span.addEventListener('click', closeModalWindow));
+
+function closeModalWindow(ev) {
+  if(firstClose ===ev.target){
+    firstModal.style.display = "none";
+  }
+  if(secondClose ===ev.target) {
+    secondModal.style.display = "none";
+  }
+  if(thirdClose ===ev.target) {
+    thirdModal.style.display = "none";
+  }
+  if(fourthClose ===ev.target) {
+    fourthModal.style.display = "none";
+  }
+  if(fifthClose ===ev.target) {
+    fifthModal.style.display = "none";
+  }
+  if(sixthClose===ev.target) {
+    sixthModal.style.display = "none";
+  }
+}
+
+
